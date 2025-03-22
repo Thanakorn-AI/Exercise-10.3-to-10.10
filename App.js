ca682c5b4cf8c3dbc10b97e9a688098e9ecdcca2
Duke <dukethanakorn@gmail.com>
2025-03-22 17:50:35 +0700

    Added Safe Area View to fix AppBar's visibility.

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 // rate-repository-app/App.js
 const { StatusBar } = require('expo-status-bar');
 const { NativeRouter } = require('react-router-native');
+const { SafeAreaProvider } = require('react-native-safe-area-context');
 const Main = require('./src/components/Main');
 
 const App = () => {
   return (
-    <>
+    <SafeAreaProvider>
       <NativeRouter>
         <Main />
       </NativeRouter>
-      <StatusBar style="auto" />
-    </>
+      <StatusBar style="light" />
+    </SafeAreaProvider>
   );
 };
 
